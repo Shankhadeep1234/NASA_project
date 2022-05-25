ba0f08f0f77ac6fc8f959f9c40f6666ec379495e
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-05-25 19:07:57 +0530

    created POST launch

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "KepLer Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("February 21, 2030"),
-  destination: "KepLer-44 b",
+  target: "KepLer-44 b",
   customer: ["NASA", "ZTM"],
   upcoming: true,
   success: true,
@@ -17,6 +19,20 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["Zero to Mastery", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
