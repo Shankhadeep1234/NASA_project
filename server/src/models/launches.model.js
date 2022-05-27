30efacebe2d39c5392476ea88c435013198b1e56
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-05-27 19:18:00 +0530

    added all launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,5 @@
-//const launchesDatabase = require("./launches.mongo");
+const launchesDataBase = require("./launches.mongo");
+const planets = require("./planets.mongo");
 
 const launches = new Map();
 
@@ -9,20 +10,45 @@ const launch = {
   mission: "KepLer Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("February 21, 2030"),
-  target: "KepLer-44 b",
-  customer: ["NASA", "ZTM"],
+  target: "Kepler-442 b",
+  customers: ["NASA", "ZTM"],
   upcoming: true,
   success: true,
 };
-
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getAllLaunches() {
+  return await launchesDataBase.find(
+    {},
+    {
+      _id: 0,
+      __v: 0,
+    }
+  );
+}
+
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("No matching planet found");
+  }
+
+  await launchesDataBase.updateOne(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
 }
 
 function addNewLaunch(launch) {
