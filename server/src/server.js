5d20be17825756450b07f3af5843fbee1dd5284c
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-05-28 10:20:57 +0530

    deleted unwanted code from server.js

--- server/src/server.js
+++ server/src/server.js
@@ -9,14 +9,6 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready..");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
   await mongoConnect();
   await loadPlanetsData();
