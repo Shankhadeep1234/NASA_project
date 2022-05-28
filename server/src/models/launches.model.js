17dae1dc2dbb35b37df2630217b16814001f7956
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-05-28 14:42:01 +0530

    added pagination feature in API params

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,20 +7,6 @@ const DEFAULT_FLIGHT_NUMBER = 100;
 
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
-const launches = new Map();
-
-const launch = {
-  flightNumber: 100,
-  mission: "KepLer Exploration X",
-  rocket: "Explorer IS1",
-  launchDate: new Date("February 21, 2030"),
-  target: "Kepler-442 b",
-  customers: ["NASA", "ZTM"],
-  upcoming: true,
-  success: true,
-};
-saveLaunch(launch);
-
 async function populateLaunches() {
   console.log("Downloading spaceX data");
   const response = await axios.post(SPACEX_API_URL, {
@@ -105,30 +91,24 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
-  return await launchesDataBase.find(
-    {},
-    {
-      _id: 0,
-      __v: 0,
-    }
-  );
-}
-
-async function saveLaunch(skip, limit) {
-  await launchesDataBase
-    .find(
-      {
-        flightNumber: launch.flightNumber,
-      },
-      launch,
-      {
-        upsert: true,
-      }
-    )
+async function getAllLaunches(skip, limit) {
+  return await launchesDataBase
+    .find({}, { _id: 0, __v: 0 })
     .sort({ flightNumber: 1 })
     .skip(skip)
     .limit(limit);
+}
+
+async function saveLaunch(launch) {
+  await launchesDataBase.findOneAndUpdate(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
 }
 
 async function scheduleNewLaunch(launch) {
